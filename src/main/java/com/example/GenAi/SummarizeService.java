4845db88da3ba1c2fa05a53aa1395e696838b7f5
satyam970 <satyamkumar28838@gmail.com>
2026-09-19 15:12:49 +0530

    making chatbot for a specific task(like here food related queries only)

--- src/main/java/com/example/GenAi/SummarizeService.java
+++ src/main/java/com/example/GenAi/SummarizeService.java
@@ -16,6 +16,22 @@ public class SummarizeService {
 
     private List<Message> history =new ArrayList<>();
 
+    private final String SYSTEM_PROMPT=
+            """
+            You are customer-support executive for our Food ordering app named Homefood.
+            
+            Your job is to identify the customer's main problem and urgency . Answer them related to their query in one line.
+            
+            Use professional language. If user has an issue , use words like I understand your frustration, I am really 
+            sorry for your trouble etc.
+            
+            Do not answer any other question which is not related to Ordering Food query, refund query, order tracking
+            status query or company policy query..
+           """;
+
+
+
+
     public SummarizeService(ChatClient.Builder builder){
         this.chatClient = builder.build();
     }
@@ -24,6 +40,7 @@ public class SummarizeService {
         history.add(new UserMessage(message));
 
         String output=chatClient.prompt()
+                .system(SYSTEM_PROMPT)
                 .messages(history)
                 .call()
                 .content();
